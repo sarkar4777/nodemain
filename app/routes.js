29740d144d7d68abef7bed7789e8f0ab39ebc54a
Tathagata Sarkar <sarkar4777@gmail.com>
2014-09-18 18:53:21 +0000

    Commit Second Batch

--- app/routes.js
+++ app/routes.js
@@ -13,4 +13,38 @@ module.exports.initialize = function(app) {
 	app.get('/useredit', user.useredit);
 	app.post('/usersave', user.usersave);
 	app.get('/get/user', ajax.getUserDataTable);
+	
+	app.get('/404', function(req, res, next){
+		next();
+	});
+	app.get('/403', function(req, res, next){
+		var err = new Error('Prohibited Material!!');
+		err.status = 403;
+		next(err);
+	});
+	app.get('/500', function(req, res, next){
+		next(new Error('keyboard cat!'));
+	});
+	
+	app.use(function(req, res, next){
+		res.status(404);
+
+		if (req.accepts('html')) {
+			res.render('404', {});
+			return;
+		}
+
+		if (req.accepts('json')) {
+			res.send({ error: 'Not found' });
+			return;
+		}
+
+		res.type('txt').send('Not found');
+	});
+	
+	app.use(function(err, req, res, next){
+		res.status(err.status || 500);
+		res.render('500', {});
+	});
+
 };
